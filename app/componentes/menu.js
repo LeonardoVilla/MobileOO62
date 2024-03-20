e54840f4390ce263ba9d675a6f90f13c3956142d
Leonardo da Silva Villa <leosivi@gmail.com>
2024-03-20 11:13:08 -0400

    Add files via upload

---
+++ app/componentes/menu.js
@@ -0,0 +1,9 @@
+function carregaMenu(caminhoMenu){
+
+    fetch(caminhoMenu)
+        .then((response) => response.text())
+        .then((html) =>{
+            document.getElementById("menu").innerHTML =html;
+        });
+
+}
