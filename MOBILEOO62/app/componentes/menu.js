a2fa728faf8816df86b5ff0229846f99f9b1c5fc
Leonardo da Silva Villa <leosivi@gmail.com>
2024-03-20 11:12:10 -0400

    Delete MOBILEOO62 directory

--- MOBILEOO62/app/componentes/menu.js
+++
@@ -1,9 +0,0 @@
-function carregaMenu(caminhoMenu){
-
-    fetch(caminhoMenu)
-        .then((response) => response.text())
-        .then((html) =>{
-            document.getElementById("menu").innerHTML =html;
-        });
-
-}
